394a17ae0c09d97d81f1a06cd7b7cda4e8a9c005
grichal <gregori7813@gmail.com>
2024-10-14 20:24:27 -0400

    firt push

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "../assets/logogrichal.png"
+import {FaLinkedin} from 'react-icons/fa'
+import {FaGithub} from 'react-icons/fa'
+import {FaInstagram} from 'react-icons/fa'
+
+function Navbar() {
+  return (
+    <nav className="md-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="" className='size-20' /></div>
+        <div className='m-8 flex items-center justify-center gap-4 text-2xl'>
+        <FaLinkedin/>
+        <FaGithub/></div>
+       </nav>
+  )
+}
+
+export default Navbar
